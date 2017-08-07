4238fc23f4f68d4d4dd361aaf97920d24a9dd801
xingzichen1688 <30788402+xingzichen1688@users.noreply.github.com>
2017-08-07 19:11:47 +0800

    新增实体类8.7
    Signed-off-by: xingzichen1688 <30788402+xingzichen1688@users.noreply.github.com>

--- consoleservice/src/main/java/com/mcylm/clay/consoleservice/entity/Ucenter.java
+++ consoleservice/src/main/java/com/mcylm/clay/consoleservice/entity/Ucenter.java
@@ -4,7 +4,7 @@ package com.mcylm.clay.consoleservice.entity;
  * Created by macbook on 2017/8/7.
  */
 public class Ucenter {
-    //邢子琛--
+    //fhfghfgj
     private Integer id;
     private String uuid;
     private String username;
