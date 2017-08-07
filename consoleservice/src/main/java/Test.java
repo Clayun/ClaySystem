d34b1e07e11cf8975a7a8210c94e7fb845766f21
xingzichen1688 <30788402+xingzichen1688@users.noreply.github.com>
2017-08-07 19:07:13 +0800

    新增实体类

--- consoleservice/src/main/java/Test.java
+++ consoleservice/src/main/java/Test.java
@@ -2,4 +2,7 @@
  * Created by Mikesam on 2017/8/7.
  */
 public class Test {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ consoleservice/src/main/java/com/mcylm/clay/consoleservice/entity/Ucenter.java
@@ -0,0 +1,79 @@
+package com.mcylm.clay.consoleservice.entity;
+
+/**
+ * Created by macbook on 2017/8/7.
+ */
+public class Ucenter {
+    private Integer id;
+    private String uuid;
+    private String username;
+    private String phone;
+    private String password;
+    private String creatTime;
+    private String permission;
+    private String bindUser;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCreatTime() {
+        return creatTime;
+    }
+
+    public void setCreatTime(String creatTime) {
+        this.creatTime = creatTime;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getBindUser() {
+        return bindUser;
+    }
+
+    public void setBindUser(String bindUser) {
+        this.bindUser = bindUser;
+    }
+}
