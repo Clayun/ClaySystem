91d61dfdfd3e57b8599e69be9676daa9ddc1fbd9
lenovo <1753241874@qq.com>
2017-08-08 13:49:28 +0800

    跳转登录页面

---
+++ securityService/src/main/java/com/mcylm/clay/securityservice/SecurityService.java
@@ -0,0 +1,19 @@
+package com.mcylm.clay.securityservice;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+/**
+ * Created by Mikesam on 2017/8/4.
+ */
+
+
+@SpringBootApplication(scanBasePackages = "com.mcylm.clay.securityservice.*")
+@MapperScan({"com.mcylm.clay.securityservice.mapper.*"})
+@EnableEurekaClient
+public class SecurityService {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityService.class, args);
+    }
+}
---
+++ securityService/src/main/java/com/mcylm/clay/securityservice/service/UauthService.java
@@ -0,0 +1,7 @@
+package com.mcylm.clay.securityservice.service;
+
+/**
+ * Created by lenovo on 2017/8/7.
+ */
+public interface UauthService {
+}
