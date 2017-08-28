41ed2fa086dfc58b2dde2b4b380075334f380354
Mikesam <1627072456@qq.com>
2017-08-28 20:09:51 +0800

    Create takeparam.js
    参数拼接工具类，功能：将token和logintype的参数追加到即将跳转页面路径后面，不建议使用a标签跳转，应使用onclick函数，参数为即将跳转的目标页面的路径，方法自动追加参数。

---
+++ staticservice/src/main/resources/static/js/takeparam.js
@@ -0,0 +1,35 @@
+/**
+ * Created by lenovo on 2017/8/28.
+ * 拼接路径的参数
+ */
+var token = "";
+var loginType = "";
+function takeparam(url) {
+    token = getQueryString("token");//获取token
+    loginType = getQueryString("loginType");//获取logintype
+    //判断是否有token
+    if (token != null && token != '') {
+        url += "?token=" + token;
+    }
+    //防止路径符号错误
+    if (url.indexOf("?") != -1) {
+        url += "?";
+    } else {
+        url += "&";
+    }
+    //获取loginType
+    if (loginType != null&&loginType!='') {
+        url += "loginType="+loginType;
+    }
+    //跳转路径
+    window.location.href="http://"+url;
+}
+
+
+//获取路径上指定的参数
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
