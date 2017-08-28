d5f152fb80207d125198f79075f98b8d90459108
lenovo <1753241874@qq.com>
2017-08-28 19:54:11 +0800

    获取页面的参数，并拼接到路径上 使用：onclick(takeparam("/sercity/author/register"))

--- staticservice/src/main/resources/static/js/takeparam.js
+++ staticservice/src/main/resources/static/js/takeparam.js
@@ -7,24 +7,27 @@ var loginType = "";
 function takeparam(url) {
     token = getQueryString("token");//获取token
     loginType = getQueryString("loginType");//获取logintype
+
     //判断是否有token
     if (token != null && token != '') {
         url += "?token=" + token;
     }
+
     //防止路径符号错误
     if (url.indexOf("?") != -1) {
         url += "?";
     } else {
         url += "&";
     }
+
     //获取loginType
     if (loginType != null&&loginType!='') {
         url += "loginType="+loginType;
     }
+
     //跳转路径
     window.location.href="http://"+url;
 }
-
 
 //获取路径上指定的参数
 function getQueryString(name) {
