9f94c98aea0e074979a6ee4be6309b7803d02b23
lenovo <1753241874@qq.com>
2017-08-30 19:01:58 +0800

    完善未登录注册，路径传参功能

--- staticservice/src/main/resources/static/js/takeparam.js
+++ staticservice/src/main/resources/static/js/takeparam.js
@@ -1,24 +1,30 @@
 var token = "";
 var loginType = "";
+var formToken = "";
 function takeparam(url) {
+    redirectUrl = getQueryString("redirectUrl");//获取token
     token = getQueryString("token");//获取token
     loginType = getQueryString("loginType");//获取logintype
+    formToken = getQueryString("formToken");//获取logintype
+
+    //判断是否有redirectUrl
+    if (redirectUrl != null && redirectUrl != '') {
+        url = checkParam(url) + "redirectUrl=" + redirectUrl;
+    }
     //判断是否有token
     if (token != null && token != '') {
-        url += "?token=" + token;
+        url = checkParam(url) + "token=" + token;
     }
-    //防止路径符号错误
-    if (url.indexOf("?") != -1) {
-        url += "?";
-    } else {
-        url += "&";
+    //判断是否有loginType
+    if (loginType != null && loginType != '') {
+        url = checkParam(url) + "loginType=" + loginType;
     }
-    //获取loginType
-    if (loginType != null&&loginType!='') {
-        url += "loginType="+loginType;
+  //获取formToken
+    if (formToken != null && formToken != '') {
+        url = checkParam(url) + "formToken=" + formToken;
     }
     //跳转路径
-    window.location.href="http://"+url;
+    window.location.href="http://localhost"+url;
 }
 
 //获取路径上指定的参数
@@ -28,5 +34,13 @@ function getQueryString(name) {
     if (r != null) return unescape(r[2]);
     return null;
 }
-
+function checkParam(url) {
+    //防止路径符号错误
+    if (url.indexOf("?") != -1) {
+        url += "&";
+    } else {
+        url += "?";
+    }
+    return url;
+}
 
